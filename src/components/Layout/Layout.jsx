3e6644870ed81a8cfb0af6db98f9a7b3856937ee
Roman Vashchenko <roman1991dp@gmail.com>
2024-10-12 16:19:13 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
-import css from "./Layout.module.css";
+import "./Layout.module.css";
 import Logo from "../Logo/Logo";
 import NavBar from "../NavBar/NavBar";
 
 const Layout = () => {
   return (
     <div>
-      <header className={css.header}>
+      <header>
         <Logo />
         <NavBar />
       </header>
