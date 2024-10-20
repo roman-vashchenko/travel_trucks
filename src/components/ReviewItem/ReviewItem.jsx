8a571f7d124c08c7ff26fcefa7dc3ee184465be4
Roman Vashchenko <roman1991dp@gmail.com>
2024-10-20 21:41:55 +0300

    update

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,11 +1,34 @@
 /* eslint-disable react/prop-types */
+import css from "./ReviewItem.module.css";
 
 const ReviewItem = ({ review }) => {
+  const totalStars = 5;
   return (
     <>
-      <div></div>
-      <p>{review.reviewer_name}</p>
-      <p>{review.comment}</p>
+      <div className={css.wrapper}>
+        <div className={css.icon}>
+          <span>{review.reviewer_name.charAt(0)}</span>
+        </div>
+        <div>
+          <p className={css.name}>{review.reviewer_name}</p>
+          <div>
+            {[...Array(totalStars)].map((_, index) => (
+              <span key={index}>
+                {index < review.reviewer_rating ? (
+                  <svg width={16} height={16}>
+                    <use href="/icons.svg#star"></use>
+                  </svg>
+                ) : (
+                  <svg width={16} height={16}>
+                    <use href="/icons.svg#star_transparent"></use>
+                  </svg>
+                )}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+      <p className={css.comment}>{review.comment}</p>
     </>
   );
 };
